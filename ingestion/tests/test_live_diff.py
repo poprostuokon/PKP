9f73e4a1d4861381ef3ca8f32f7dae2fa6f75130
Bartłomiej Okoniewski <bartlomiej.okoniewski@gmail.com>
2026-09-18 08:58:15 +0200

    dodanie kolejnych testów dla pytest

--- ingestion/tests/test_live_diff.py
+++ ingestion/tests/test_live_diff.py
@@ -1,5 +1,6 @@
 from pkp_ingestion.live_diff import _canon, diff_operations
 from pkp_ingestion.live_diff import diff_disruptions
+from pkp_ingestion.live_diff import _op_tracked
 
 SID = 60103
 
@@ -61,3 +62,26 @@ def test_disr_zniknal_stary_nie_ended():
     changed, ended = diff_disruptions([_disr(route_date="2026-09-10")], [],
                                       ended_min_date="2026-09-17")
     assert ended == []
+
+# 1) zmiana niefinalnego rekordu -> leci (brakowało tej ścieżki)
+def test_zmiana_niefinalny_leci():
+    prev = _train(actual="08:05")
+    curr = _train(actual="08:09")   # inny actual, niefinalny
+    assert len(diff_operations([prev], [curr], SID)) == 1
+
+# 2) pole nieobecne == false -> ten sam hash (ochrona przed fałszywym diffem)
+def test_absent_false_ten_sam_hash():
+    a = {"trainStatus": "R", "stations": [{"stationId": SID, "plannedArrival": "08:00"}]}
+    b = {"trainStatus": "R", "stations": [{"stationId": SID, "plannedArrival": "08:00",
+                                           "isConfirmed": False, "isCancelled": False}]}
+    assert _canon(_op_tracked(a, SID)) == _canon(_op_tracked(b, SID))
+
+# 3) disruptions: zmiana typu (nie tylko message) -> changed
+def test_disr_zmiana_typu_leci():
+    d1 = {"disruptionTypeCode": "TECH", "message": "x",
+          "affectedRoutes": [{"scheduleId": 1, "orderId": 10, "trainOrderId": 5,
+                              "operatingDate": "2026-09-17", "stationId": 60103,
+                              "sequenceNumber": 1}]}
+    d2 = {**d1, "disruptionTypeCode": "STRAJK"}
+    changed, _ = diff_disruptions([d1], [d2])
+    assert len(changed) == 1
